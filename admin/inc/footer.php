1eedeb744326fe2f05f017d6b2486a7c76f78e76
AnisurCode <86513932+AnisurCode@users.noreply.github.com>
2022-10-31 23:08:00 +0600

    first commit

---
+++ admin/inc/footer.php
@@ -0,0 +1,12 @@
+<div class="clear">
+        </div>
+    </div>
+    <div class="clear">
+    </div>
+    <div id="site_info">
+        <p>
+         &copy; Copyright 19CSE046_Anisur Rahman. All Rights Reserved.
+        </p>
+    </div>
+</body>
+</html>
---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "electronic_products_shop");
+define("TITLE", "Electronic Products Shop");
+define("KEYWORDS", "Fan,mobile");
+?>
